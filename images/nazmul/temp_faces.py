f70600444b05129ae45c232f583ec45ecab588ff
Nazmul Hassan <nazmulhasannoman111@gmail.com>
2020-04-18 02:12:42 +0600

    face_&_eye

---
+++ images/nazmul/temp_faces.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+import pickle
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trianner.yml")
+
+labels = {"person_name": 1}
+with open('labels.pickle', 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+cnt = 0
+con=0
+x1 = np.array([0,1,2])
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=45:
+            print(id_)
+            fram1 = id_
+            print(labels[id_])
+            name = labels[id_]
+            # x1[fram1]=1
+            # i=0
+            # for i in x1:
+            #     if x1[i]==1:
+            #         con+=1
+            # print(con)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item = "nazmul"+str(cnt)+".png"
+        cv2.imwrite(img_item, roi_gray)
+        color = (255, 0, 0)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            roi_gray = gray[ey:ey + eh, ex:ex + ew]
+            print(ex, ey, ew, eh)
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        cnt+=1
+
+    cv2.imshow('frame',frame)
+    con = 0
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
